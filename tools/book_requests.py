68efc6d07611bed94f59bc18fad20ac8436ef633
KingJ <501315555@qq.com>
2019-02-19 19:57:26 +0800

    little change...
    little change...

--- tools/book_requests.py
+++ tools/book_requests.py
@@ -6,17 +6,17 @@ conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="xici_
 cursor = conn.cursor()
 
 
-def crawl_books(search_text):
+def crawl_books(douban_id):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0"}
 
-    param = {"search_text": search_text, "cat": "1001"}
     # re = requests.get("https://book.douban.com/subject_search?search_text=%E6%B4%BB%E7%9D%80&cat=1001")
-    re = requests.get("https://book.douban.com/subject_search", params=param, headers=headers)
+    url = "https://book.douban.com/subject/" + douban_id + "/"
+    re = requests.get(url, headers=headers)
 
     selector = Selector(text=re.text)
-    link = selector.css('#root .item-root a::attr(href)').extract()
+    link = selector.css('#wrapper h1 span::text').extract()
     print(link)
 
 
 if __name__ == "__main__":
-    crawl_books(9787506365437)
+    crawl_books("25862578")
