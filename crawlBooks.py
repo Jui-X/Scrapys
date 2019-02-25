4823e59eea2f2ad273c959172d78d97402b3d834
KingJ <501315555@qq.com>
2019-02-25 11:08:15 +0800

    2019-2-25
    Crawl Books

--- crawlBooks.py
+++
@@ -1,9 +0,0 @@
-#根据豆瓣id爬取某本书的数据
-#@INPUT：douban_id
-#@OUTPUT: 表示图书信息的json，包括：
-# douban_id,title,author,publishing_house,rating
-# tag, description, rec_book,review_tag,review_content
-def crawlABook(douban_id):
-    # TODO
-    book = {'id': 1,'title': 'name'}
-    return book
---
+++ tools/crawlBooks.py
@@ -0,0 +1,49 @@
+import requests
+from scrapy.selector import Selector
+import MySQLdb
+
+# 根据豆瓣id爬取某本书的数据
+# @INPUT：douban_id
+# @OUTPUT: 表示图书信息的json，包括：
+# douban_id,title,author,publishing_house,rating
+# tag, description, rec_book,review_tag,review_content
+
+
+def crawlABook(douban_id):
+    # TODO
+    conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="xici_ip", charset="utf8")
+    cursor = conn.cursor()
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0"}
+
+    url = "https://book.douban.com/subject/" + douban_id + "/"
+    re = requests.get(url, headers=headers)
+
+    selector = Selector(text=re.text)
+    # link = selector.css('#wrapper h1 span::text').extract()
+    title = selector.css('#wrapper h1 span::text').extract()[0]
+    rating = selector.css('.rating_self strong::text').extract()[0].strip()
+    tag = selector.css('.indent span .tag::text').extract()
+    description = selector.css('.related_info .intro p::text').extract()[0:2]
+    rec_books = selector.css('#db-rec-section .content dl dd a::text').extract()
+    rec_book_list = []
+    for rec_book in rec_books:
+        rec_book_list.append(rec_book.replace('\n', '').strip())
+
+    review_url = selector.css('.review-list .review-item h2 a::attr(href)').extract()
+    review_content = []
+    for i in range(0, 5):
+        review = requests.get(review_url[i], headers=headers)
+        review_selector = Selector(text=review.text)
+        reviews = review_selector.css('.review-content::text').extract()
+        for review in reviews:
+            review_content.append(review.replace('\n', '').replace('\t', '').replace('\xa0', '').strip())
+
+    book = {'id': douban_id, 'title': title, 'rating': rating, 'tag': tag, 'description': description,
+            'rec_books': rec_books, 'review_content': review_content}
+
+    return book
+
+# if __name__ == "__main__":
+#     crawlABook("4913064")
+
